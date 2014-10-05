6ada365192a29ae53a583979c052df0652f722e9
edgarfelizmenio <edgarfelizmenio@gmail.com>
2014-10-05 23:46:39 +0800

    added table definition for months

---
+++ months.sql
@@ -0,0 +1,18 @@
+CREATE TABLE months (
+    "MonthID" smallint,
+    "MonthName" character varying(10)
+);
+
+INSERT INTO months VALUES
+(1, 'January'),
+(2, 'February'),
+(3, 'March'),
+(4, 'April'),
+(5, 'May'),
+(6, 'June'),
+(7, 'July'),
+(8, 'August'),
+(9, 'September'),
+(10, 'October'),
+(11, 'November'),
+(12, 'December');
